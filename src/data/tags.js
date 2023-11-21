a25d4e45ab3f41ab469d61647f91706ba6569f4e
Jun <17263194+junlqu@users.noreply.github.com>
2023-11-21 04:10:02 -0500

    feat: added all tags

---
+++ src/data/tags.js
@@ -0,0 +1,36 @@
+const tags = [
+  // The ranges
+  "Melee",
+  "Ranged",
+  // The stars
+  "Robot",
+  "Starter",
+  "Senior Operator",
+  "Top Operator",
+  // The archetype
+  "Guard",
+  "Sniper",
+  "Defender",
+  "Medic",
+  "Supporter",
+  "Caster",
+  "Specialist",
+  "Vanguard",
+  // The roles
+  "Crowd Control",
+  "Nuker",
+  "Healing",
+  "Support",
+  "DP-Recovery",
+  "DPS",
+  "Survival",
+  "AoE",
+  "Defense",
+  "Slow",
+  "Debuff",
+  "Fast-Redeploy",
+  "Shift",
+  "Summon"
+];
+
+export default tags;
