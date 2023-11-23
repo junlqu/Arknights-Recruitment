d636efd11c067f12258200a77d93957fac2e6e4a
Jun <17263194+junlqu@users.noreply.github.com>
2023-11-23 03:24:27 -0500

    feat: added a mapping to the character images

---
+++ src/data/imageMatching.js
@@ -0,0 +1,247 @@
+import ThermalEX from "./images/Thermal-EX.png";
+import Castle3 from "./images/Castle-3.png";
+import JusticeKnight from "./images/JusticeKnight.png";
+import Lancet2 from "./images/Lancet-2.png";
+import NoirCorne from "./images/NoirCorne.png";
+import Yato from "./images/Yato.png";
+import Rangers from "./images/Rangers.png";
+import F12 from "./images/12F.png";
+import Durin from "./images/Durin.png";
+import Spot from "./images/Spot.png";
+import Popukar from "./images/Popukar.png";
+import Midnight from "./images/Midnight.png";
+import Beagle from "./images/Beagle.png";
+import Melantha from "./images/Melantha.png";
+import Plume from "./images/Plume.png";
+import Vanilla from "./images/Vanilla.png";
+import Fang from "./images/Fang.png";
+import Catapult from "./images/Catapult.png";
+import Orchid from "./images/Orchid.png";
+import Adnachiel from "./images/Adnachiel.png";
+import Kroos from "./images/Kroos.png";
+import Steward from "./images/Steward.png";
+import Lava from "./images/Lava.png";
+import Ansel from "./images/Ansel.png";
+import Hibiscus from "./images/Hibiscus.png";
+import Cutter from "./images/Cutter.png";
+import Utage from "./images/Utage.png";
+import Myrtle from "./images/Myrtle.png";
+import Matterhorn from "./images/Matterhorn.png";
+import Gummy from "./images/Gummy.png";
+import Cuora from "./images/Cuora.png";
+import Matoimaru from "./images/Matoimaru.png";
+import Frostleaf from "./images/Frostleaf.png";
+import Mousse from "./images/Mousse.png";
+import Beehunter from "./images/Beehunter.png";
+import Estelle from "./images/Estelle.png";
+import Dobermann from "./images/Dobermann.png";
+import Shaw from "./images/Shaw.png";
+import Rope from "./images/Rope.png";
+import Gravel from "./images/Gravel.png";
+import Vigna from "./images/Vigna.png";
+import Scavenger from "./images/Scavenger.png";
+import Click from "./images/Click.png";
+import Podenco from "./images/Podenco.png";
+import Purestream from "./images/Purestream.png";
+import Ambriel from "./images/Ambriel.png";
+import May from "./images/May.png";
+import Vermeil from "./images/Vermeil.png";
+import Sussurro from "./images/Sussurro.png";
+import Greyy from "./images/Greyy.png";
+import Earthspirit from "./images/Earthspirit.png";
+import Jessica from "./images/Jessica.png";
+import Meteor from "./images/Meteor.png";
+import ShiraYuki from "./images/ShiraYuki.png";
+import Gitano from "./images/Gitano.png";
+import Haze from "./images/Haze.png";
+import Perfumer from "./images/Perfumer.png";
+import Myrrh from "./images/Myrrh.png";
+import Ayerscape from "./images/Ayerscape.png";
+import Asbestos from "./images/Asbestos.png";
+import Elysium from "./images/Elysium.png";
+import Hung from "./images/Hung.png";
+import Broca from "./images/Broca.png";
+import Reed from "./images/Reed.png";
+import WaaiFu from "./images/WaaiFu.png";
+import Astesia from "./images/Astesia.png";
+import Swire from "./images/Swire.png";
+import Croissant from "./images/Croissant.png";
+import Vulcan from "./images/Vulcan.png";
+import Liskarm from "./images/Liskarm.png";
+import Nearl from "./images/Nearl.png";
+import Indra from "./images/Indra.png";
+import Specter from "./images/Specter.png";
+import FEater from "./images/FEater.png";
+import Cliffheart from "./images/Cliffheart.png";
+import Manticore from "./images/Manticore.png";
+import ProjektRed from "./images/ProjektRed.png";
+import Zima from "./images/Zima.png";
+import Texas from "./images/Texas.png";
+import Leonhardt from "./images/Leonhardt.png";
+import Tsukinogi from "./images/Tsukinogi.png";
+import Shamare from "./images/Shamare.png";
+import Sesa from "./images/Sesa.png";
+import Leizi from "./images/Leizi.png";
+import GreyThroat from "./images/GreyThroat.png";
+import Executor from "./images/Executor.png";
+import Glaucus from "./images/Glaucus.png";
+import Mayer from "./images/Mayer.png";
+import Istina from "./images/Istina.png";
+import Pramanix from "./images/Pramanix.png";
+import Platinum from "./images/Platinum.png";
+import Meteorite from "./images/Meteorite.png";
+import Firewatch from "./images/Firewatch.png";
+import BluePoison from "./images/BluePoison.png";
+import Provence from "./images/Provence.png";
+import Nightmare from "./images/Nightmare.png";
+import Warfarin from "./images/Warfarin.png";
+import Silence from "./images/Silence.png";
+import Ptilopsis from "./images/Ptilopsis.png";
+import Weedy from "./images/Weedy.png";
+import Phantom from "./images/Phantom.png";
+import Bagpipe from "./images/Bagpipe.png";
+import Blaze from "./images/Blaze.png";
+import Hellagur from "./images/Hellagur.png";
+import Skadi from "./images/Skadi.png";
+import Saria from "./images/Saria.png";
+import Hoshiguma from "./images/Hoshiguma.png";
+import SilverAsh from "./images/SilverAsh.png";
+import Siege from "./images/Siege.png";
+import Chen from "./images/Chen.png";
+import Suzuran from "./images/Suzuran.png";
+import Rosa from "./images/Rosa.png";
+import Ceobe from "./images/Ceobe.png";
+import Aak from "./images/Aak.png";
+import Mostima from "./images/Mostima.png";
+import Magallan from "./images/Magallan.png";
+import Schwarz from "./images/Schwarz.png";
+import Exusiai from "./images/Exusiai.png";
+import Ifrit from "./images/Ifrit.png";
+import Nightingale from "./images/Nightingale.png";
+import Shining from "./images/Shining.png";
+
+const imageMatch = {
+  "Thermal-EX": "./images/Thermal-EX.png",
+  "Castle-3": "./images/Castle-3.png",
+  "Justice Knight": "./images/Justice Knight.png",
+  "Lancet-2": "./images/Lancet-2.png",
+  "Noire Corne": "./images/Noire Corne.png",
+  "Yato": "./images/Yato.png",
+  "Rangers": "./images/Rangers.png",
+  "12F": "./images/12F.png",
+  "Durin": "./images/Durin.png",
+  "Spot": "./images/Spot.png",
+  "Popukar": "./images/Popukar.png",
+  "Midnight": "./images/Midnight.png",
+  "Beagle": "./images/Beagle.png",
+  "Melantha": "./images/Melantha.png",
+  "Plume": "./images/Plume.png",
+  "Vanilla": "./images/Vanilla.png",
+  "Fang": "./images/Fang.png",
+  "Catapult": "./images/Catapult.png",
+  "Orchid": "./images/Orchid.png",
+  "Adnachiel": "./images/Adnachiel.png",
+  "Kroos": "./images/Kroos.png",
+  "Steward": "./images/Steward.png",
+  "Lava": "./images/Lava.png",
+  "Ansel": "./images/Ansel.png",
+  "Hibiscus": "./images/Hibiscus.png",
+  "Cutter": "./images/Cutter.png",
+  "Utage": "./images/Utage.png",
+  "Myrtle": "./images/Myrtle.png",
+  "Matterhorn": "./images/Matterhorn.png",
+  "Gummy": "./images/Gummy.png",
+  "Cuora": "./images/Cuora.png",
+  "Matoimaru": "./images/Matoimaru.png",
+  "Frostleaf": "./images/Frostleaf.png",
+  "Mousse": "./images/Mousse.png",
+  "Beehunter": "./images/Beehunter.png",
+  "Estelle": "./images/Estelle.png",
+  "Dobermann": "./images/Dobermann.png",
+  "Shaw": "./images/Shaw.png",
+  "Rope": "./images/Rope.png",
+  "Gravel": "./images/Gravel.png",
+  "Vigna": "./images/Vigna.png",
+  "Scavenger": "./images/Scavenger.png",
+  "Click": "./images/Click.png",
+  "Podenco": "./images/Podenco.png",
+  "Purestream": "./images/Purestream.png",
+  "Ambriel": "./images/Ambriel.png",
+  "May": "./images/May.png",
+  "Vermeil": "./images/Vermeil.png",
+  "Sussurro": "./images/Sussurro.png",
+  "Greyy": "./images/Greyy.png",
+  "Earthspirit": "./images/Earthspirit.png",
+  "Jessica": "./images/Jessica.png",
+  "Meteor": "./images/Meteor.png",
+  "ShiraYuki": "./images/ShiraYuki.png",
+  "Gitano": "./images/Gitano.png",
+  "Haze": "./images/Haze.png",
+  "Perfumer": "./images/Perfumer.png",
+  "Myrrh": "./images/Myrrh.png",
+  "Ayerscape": "./images/Ayerscape.png",
+  "Asbestos": "./images/Asbestos.png",
+  "Elysium": "./images/Elysium.png",
+  "Hung": "./images/Hung.png",
+  "Broca": "./images/Broca.png",
+  "Reed": "./images/Reed.png",
+  "Waai Fu": "./images/Waai Fu.png",
+  "Astesia": "./images/Astesia.png",
+  "Swire": "./images/Swire.png",
+  "Croissant": "./images/Croissant.png",
+  "Vulcan": "./images/Vulcan.png",
+  "Liskarm": "./images/Liskarm.png",
+  "Nearl": "./images/Nearl.png",
+  "Indra": "./images/Indra.png",
+  "Specter": "./images/Specter.png",
+  "FEater": "./images/FEater.png",
+  "Cliffheart": "./images/Cliffheart.png",
+  "Manticore": "./images/Manticore.png",
+  "Projekt Red": "./images/Projekt Red.png",
+  "Zima": "./images/Zima.png",
+  "Texas": "./images/Texas.png",
+  "Leonhardt": "./images/Leonhardt.png",
+  "Tsukinogi": "./images/Tsukinogi.png",
+  "Shamare": "./images/Shamare.png",
+  "Sesa": "./images/Sesa.png",
+  "Leizi": "./images/Leizi.png",
+  "GreyThroat": "./images/GreyThroat.png",
+  "Executor": "./images/Executor.png",
+  "Glaucus": "./images/Glaucus.png",
+  "Mayer": "./images/Mayer.png",
+  "Istina": "./images/Istina.png",
+  "Pramanix": "./images/Pramanix.png",
+  "Platinum": "./images/Platinum.png",
+  "Meteorite": "./images/Meteorite.png",
+  "Firewatch": "./images/Firewatch.png",
+  "Blue Poison": "./images/Blue Poison.png",
+  "Provence": "./images/Provence.png",
+  "Nightmare": "./images/Nightmare.png",
+  "Warfarin": "./images/Warfarin.png",
+  "Silence": "./images/Silence.png",
+  "Ptilopsis": "./images/Ptilopsis.png",
+  "Weedy": "./images/Weedy.png",
+  "Phantom": "./images/Phantom.png",
+  "Bagpipe": "./images/Bagpipe.png",
+  "Blaze": "./images/Blaze.png",
+  "Hellagur": "./images/Hellagur.png",
+  "Skadi": "./images/Skadi.png",
+  "Saria": "./images/Saria.png",
+  "Hoshiguma": "./images/Hoshiguma.png",
+  "SilverAsh": "./images/SilverAsh.png",
+  "Siege": "./images/Siege.png",
+  "Ch'en": "./images/Ch'en.png",
+  "Suzuran": "./images/Suzuran.png",
+  "Rosa": "./images/Rosa.png",
+  "Ceobe": "./images/Ceobe.png",
+  "Aak": "./images/Aak.png",
+  "Mostima": "./images/Mostima.png",
+  "Magallan": "./images/Magallan.png",
+  "Schwarz": "./images/Schwarz.png",
+  "Exusiai": "./images/Exusiai.png",
+  "Ifrit": "./images/Ifrit.png",
+  "Nightingale": "./images/Nightingale.png",
+  "Shining": "./images/Shining.png"
+}
+
+export default imageMatch;
