f2327f92bb485fd5c7289fa59637abe8acaac638
Jun <17263194+junlqu@users.noreply.github.com>
2023-11-23 03:37:11 -0500

    feat: added a test for image loading

--- src/components/main.js
+++ src/components/main.js
@@ -2,11 +2,19 @@ import { useState } from "react";
 import Recruit from "./recruit";
 import Upload from "./upload";
 
+import imageMatching from "../data/imageMatching";
+
 const Main = () => {
   const [selected, setSelect] = useState(0);
+  const [image, setImage] = useState();
 
   function toggleSelect() {
     setSelect((selected + 1) % 2);
+  }
+
+  function random() {
+    var keys = Object.keys(imageMatching);
+    setImage(imageMatching[keys[keys.length * Math.random() << 0]]);
   }
 
   return (
@@ -14,9 +22,13 @@ const Main = () => {
       <button onClick={toggleSelect}>
         Click me
       </button>
+      <button onClick={random}>
+        Random image
+      </button>
       {selected === 0 ? 
       <Recruit /> : 
       <Upload />}
+      <img src={image} alt="img"/>
     </>
   )
 }
