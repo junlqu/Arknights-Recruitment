ab4dfbc957ff4af13a386dd68cb07d1aaddd58ff
Jun <17263194+junlqu@users.noreply.github.com>
2023-11-23 03:23:53 -0500

    bug: removed an unneeded function

--- src/App.js
+++ src/App.js
@@ -10,16 +10,11 @@ function getCount() {
   return counts;
 }
 
-function rows(star, count) {
-  return(<p>{star} Star Count: {count}</p>)
-}
-
 const App = () => {
   let count = getCount();
-
   return (
     <div className="App">
-      {count.map((c, i) => rows(i + 1, c))}
+      {count.map((c, i) => <p key={i}>{i + 1} Star Count: {c}</p>)}
       <Main />
     </div>
   );
