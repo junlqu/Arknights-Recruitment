1cf8e9e918b651a3b1162ccc304a76f330fecc3c
Jun <17263194+junlqu@users.noreply.github.com>
2023-11-27 01:30:20 -0500

    bug: removed testing values

--- src/App.js
+++ src/App.js
@@ -1,20 +1,9 @@
-import units from "./data/units.json";
-
 import Main from "./components/main";
-
-function getCount() {
-  let counts = [0, 0, 0, 0, 0, 0];
-  Object.keys(units).forEach(function(key){
-    counts[units[key].Star - 1]++;
-  })
-  return counts;
-}
+import "./css/main.scss";
 
 const App = () => {
-  let count = getCount();
   return (
     <div className="App">
-      {count.map((c, i) => <p key={i}>{i + 1} Star Count: {c}</p>)}
       <Main />
     </div>
   );
