b5a78f720c3a5600fa3d4ddeecc2a5c424e30c64
Jun <17263194+junlqu@users.noreply.github.com>
2023-11-27 01:30:40 -0500

    feat: added recruitment component

--- src/components/recruit.js
+++ src/components/recruit.js
@@ -1,8 +1,66 @@
+import { useState } from "react";
+
+import tags from "../data/tags";
+
 import Filter from "./filter";
 
 const Recruit = () => {
-  console.log(Filter(["DPS", "Caster", "Guard"]))
-  return (<p>Recruit</p>)
+  const [selected, setSelected] = useState([]);
+  
+  // Adds or removes a tag from the selected list
+  function changeSelected(tag) {
+    let index = selected.indexOf(tag);
+    let temp = [];
+    if (index === -1) {
+      temp = [...selected];
+      temp = temp.concat([tag]);
+      if (selected.length === 3) {
+        temp.shift();
+      }
+    }
+    else {
+      temp = [...selected];
+      temp.splice(index, 1);
+    }
+    setSelected(temp);
+  }
+  
+  // Gets the set of tags as [Range], [Rarity], [Class], [Feature]
+  let cat_names = ["Range", "Rarity", "Class", "Feature"];
+  let categories = [[], [], [], []];
+  for (let i = 0; i < tags.length; i++) {
+    let slot = 0;
+    if (i >= 2) slot = 1;
+    if (i >= 6) slot = 2;
+    if (i >= 14) slot = 3;
+    categories[slot] = categories[slot].concat(tags[i]);
+  }
+  
+  return (
+  <>
+    <h1>
+      Recruitment Tags
+    </h1>
+    <div id="recruitment">
+      {cat_names.map((name, index) =>
+        <div className="sections" key={name} >
+          <div className="categories">
+            <h2>{name}</h2>
+          </div>
+          <div className="contents">
+            {categories[index].map((tag) =>
+              selected.includes(tag) ?
+              <button className="unselect" key={tag} onClick={() => changeSelected(tag)}>{tag}</button>
+              :
+              <button className="select" key={tag} onClick={() => changeSelected(tag)}>{tag}</button>
+            )}
+          </div>
+        </div>
+      )}
+      <button className="reset" onClick={() => setSelected([])}>Reset Tags</button>
+    </div>
+  </>
+  )
 }
 
 export default Recruit;
