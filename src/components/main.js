ec93052b010c04d4efaf9a570c80fe708fa3f793
Jun <17263194+junlqu@users.noreply.github.com>
2023-11-29 08:09:42 -0500

    fix: updated main page to remove initial testing components

--- src/components/main.js
+++ src/components/main.js
@@ -2,33 +2,21 @@ import { useState } from "react";
 import Recruit from "./Recruit";
 import Upload from "./Upload";
 
-import imageMatching from "../data/imageMatching";
-
 const Main = () => {
   const [selected, setSelect] = useState(0);
-  const [image, setImage] = useState();
 
   function toggleSelect() {
     setSelect((selected + 1) % 2);
   }
 
-  function random() {
-    var keys = Object.keys(imageMatching);
-    setImage(imageMatching[keys[keys.length * Math.random() << 0]]);
-  }
-
   return (
     <>
-      <button onClick={() => toggleSelect()}>
-        Click me
-      </button>
-      <button onClick={() => random()}>
-        Random image
-      </button>
       {selected === 0 ? 
       <Recruit /> : 
       <Upload />}
-      <img src={image} alt="img"/>
+      <button onClick={() => toggleSelect()}>
+        Click me
+      </button>
     </>
   )
 }
